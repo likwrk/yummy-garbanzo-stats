ad7fe97976914e6d5c76eae86860264f3dc563d0
likwrk <likwrk@gmail.com>
2016-10-22 22:51:51 +0300

    add resutls per parallel and per class

--- index.php
+++ index.php
@@ -4,10 +4,11 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__.'/vendor/autoload.php';
-//use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\HttpFoundation\Response;
 
-$appUrl = "http://test.wp/stats";
+spl_autoload_register(function ($class_name) {
+    if (file_exists($class_name . '.php')) include $class_name . '.php';
+});
+
 $app = new Silex\Application();
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
@@ -23,34 +24,17 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-require_once "data.php";
-require_once "addController.php";
-require_once "indexController.php";
-require_once "resultsController.php";
-require_once "deleteController.php";
-require_once "perclassController.php";
-
-$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
-    global $questions, $keys, $appUrl, $results, $keysChar, $question_titles, $question_options;
-    $twig->addGlobal('questions', $questions);
-    $twig->addGlobal('question_titles', $question_titles);
-    $twig->addGlobal('question_options', $question_options);
-    $twig->addGlobal('keys', $keys);
-    $twig->addGlobal('appUrl', $appUrl);
-    $twig->addGlobal('keysChar', $keysChar);
-    $twig->addGlobal('results', $results);
+$app['twig'] = $app->share($app->extend('twig', function(Twig_Environment $twig, \Silex\Application $app) {
+    $data = get_object_vars(AppData::Instance());
+    foreach ($data as $key => $value) {
+        $twig->addGlobal($key, $value);
+    }
     return $twig;
 }));
+
 $app['debug'] = true;
 
-$app->post('/add', $addController);
-
-$app->get('/', $indexController);
-
-$app->get('/results', $resultsController);
-
-$app->get('/results/{klass}', $perclassController);
-
-$app->get('/delete/{id}', $deleteController);
+$app->mount('/', new IndexControllerProvider());
+$app->mount('/results', new ResultsControllerProvider());
 
 $app->run();
